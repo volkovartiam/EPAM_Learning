589c5f2ff956852a86ce940a040c97bcc58d8f5a
Artem Volkov <volkov_artiam@mail.ru>
2021-12-10 16:36:38 +0200

    Hello world commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("Sum = " + c);
+
+        System.out.println("a > b = " + (a>b));
+        System.out.println("a < b = " + (a<b));
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + " ");
+
+        System.out.println("Hello world");
+    }
+}
